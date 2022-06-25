533b01a69135126d304f3700c0b3e2c869a97cf5
Luis René Panjón <luchorene97@gmail.com>
2022-06-25 18:38:47 -0500

    create DTO to order

---
+++ apps/orders/src/dto/create-order.request.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsPhoneNumber, IsPositive, IsString } from "class-validator";
+
+export class CreateOrderRequest {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsPositive()
+    @IsNotEmpty() 
+    price: number;
+
+    @IsPhoneNumber()
+    phoneNumber: string;
+
+}
