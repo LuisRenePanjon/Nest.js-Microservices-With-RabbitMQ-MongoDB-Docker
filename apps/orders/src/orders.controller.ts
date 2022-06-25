a793adc057ee1595101bf277d9065e97f7ad7368
Luis René Panjón <luchorene97@gmail.com>
2022-06-25 18:37:42 -0500

    create API to create order

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { CreateOrderRequest } from './dto/create-order.request';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  @Post()
+  async createOrder(@Body() body: CreateOrderRequest) {
+    return this.ordersService.createOrder(body);
   }
 }
